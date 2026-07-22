3cb2ef3010a3a2e7387bc10e2cde65cbf230dd2d
certifiedjoes-art <certifiedjoes@gmail.com>
2026-07-22 16:39:37 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Bump this version string every time index.html changes, so phones
 // reliably pick up the new version instead of getting stuck on an old one.
-const CACHE_NAME = 'harder-work-orders-v5';
+const CACHE_NAME = 'harder-work-orders-v6';
 
 const APP_SHELL = [
   './',
